47d9121944153519d534a995e801de9641af1f43
Alina <alinavik93@gmail.com>
2025-06-02 15:12:19 +0500

    ветка develop

--- microservice_calculate/src/main/java/com/javacode/microservice_calculate/MicroserviceCalculateApplication.java
+++ microservice_calculate/src/main/java/com/javacode/microservice_calculate/MicroserviceCalculateApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MicroserviceCalculateApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceCalculateApplication.class, args);
     }
-
 }
