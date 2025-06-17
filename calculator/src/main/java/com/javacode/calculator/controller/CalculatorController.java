f79b555a18fc5e5ef3556c908c394f6536484c8a
Alina <alinavik93@gmail.com>
2025-06-17 18:29:42 +0500

    добавление swagger

--- calculator/src/main/java/com/javacode/calculator/controller/CalculatorController.java
+++ calculator/src/main/java/com/javacode/calculator/controller/CalculatorController.java
@@ -5,6 +5,8 @@ import com.javacode.calculator.dto.LoanOfferDto;
 import com.javacode.calculator.dto.LoanStatementRequestDto;
 import com.javacode.calculator.dto.ScoringDataDto;
 import com.javacode.calculator.service.CalculatorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/calculator")
+@Tag(name = "Calculator API", description = "Микросервис для расчета кредитных предложений и условий")
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
@@ -21,11 +24,19 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
+    @Operation(
+            summary = "Рассчитать кредитные предложения",
+            description = "Возвращает список возможных кредитных предложений на основе заявки"
+    )
     @PostMapping("/offers")
     public List<LoanOfferDto> calculateOffers(@RequestBody LoanStatementRequestDto request) {
         return calculatorService.calculateOffers(request);
     }
 
+    @Operation(
+            summary = "Рассчитать условия кредита",
+            description = "Возвращает детализированные условия кредита после скоринга"
+    )
     @PostMapping("/calc")
     public CreditDto calculateCredit(@RequestBody ScoringDataDto scoringDataDto) {
         return calculatorService.calculateCredit(scoringDataDto);
