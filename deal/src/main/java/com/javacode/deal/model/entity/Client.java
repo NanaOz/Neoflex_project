6b4dfd1da7caafe0d4f88d20adc8c7f62e3ef1e6
Alina <alinavik93@gmail.com>
2025-06-29 19:41:48 +0500

    сущности

---
+++ deal/src/main/java/com/javacode/deal/model/entity/Client.java
@@ -0,0 +1,69 @@
+package com.javacode.deal.model.entity;
+
+import com.javacode.deal.model.enums.Gender;
+import com.javacode.deal.model.enums.MaritalStatus;
+import com.javacode.deal.model.jsonb.Employment;
+import com.javacode.deal.model.jsonb.Passport;
+import com.javacode.deal.model.jsonb.converter.JsonbConverter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+@Table(name = "client")
+public class Client {
+    @Id
+    @Column(name = "client_id")
+    private UUID clientId;
+
+    @Column(name = "last_name", nullable = false, length = 20)
+    private String lastName;
+
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+
+    @Column(name = "middle_name", length = 20)
+    private String middleName;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", length = 20)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "marital_status", length = 20)
+    private MaritalStatus maritalStatus;
+
+    @Column(name = "dependent_amount")
+    private Integer dependentAmount;
+
+    @Convert(converter = JsonbConverter.class)
+    @Column(name = "passport", columnDefinition = "jsonb")
+    private Passport passport;
+
+    @Convert(converter = JsonbConverter.class)
+    @Column(name = "employment", columnDefinition = "jsonb")
+    private Employment employment;
+
+    @Column(name = "account_number", unique = true, length = 34)
+    private String accountNumber;
+}
---
+++ deal/src/main/java/com/javacode/deal/model/entity/Statement.java
@@ -0,0 +1,76 @@
+package com.javacode.deal.model.entity;
+
+import com.javacode.deal.dto.LoanOfferDto;
+import com.javacode.deal.model.enums.ApplicationStatus;
+import com.javacode.deal.model.jsonb.LoanOffer;
+import com.javacode.deal.model.jsonb.StatusHistory;
+import com.javacode.deal.model.jsonb.converter.JsonbConverter;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "statement")
+public class Statement {
+    @Id
+    @Column(name = "statement_id", nullable = false, updatable = false)
+    @NotNull
+    private UUID statementId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "client_id", referencedColumnName = "client_id", nullable = false)
+    private Client client;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "credit_id", referencedColumnName = "credit_id")
+    private Credit credit;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    @NotNull
+    private ApplicationStatus status;
+
+    @Column(name = "creation_date", nullable = false)
+    @NotNull
+    private Timestamp creationDate;
+
+    @Convert(converter = JsonbConverter.class)
+    @Column(name = "applied_offer", columnDefinition = "jsonb")
+    private LoanOffer appliedOffer;
+
+    @Column(name = "sign_date")
+    private Timestamp signDate;
+
+    @Column(name = "ses_code")
+    private String sesCode;
+
+    @Convert(converter = JsonbConverter.class)
+    @Column(name = "status_history", columnDefinition = "jsonb")
+    @Builder.Default
+    private List<StatusHistory> statusHistory = new ArrayList<>();
+
+    public void addStatusHistory(StatusHistory history) {
+        if (statusHistory == null) {
+            statusHistory = new ArrayList<>();
+        }
+        statusHistory.add(history);
+    }
+}
