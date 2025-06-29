571828cc7843a3cd69710bb77b0ebee0028cbbd1
Alina <alinavik93@gmail.com>
2025-06-29 21:05:55 +0500

    добавила аннотации репозитория

--- deal/src/main/java/com/javacode/deal/repository/StatementRepository.java
+++ deal/src/main/java/com/javacode/deal/repository/StatementRepository.java
@@ -2,8 +2,10 @@ package com.javacode.deal.repository;
 
 import com.javacode.deal.model.entity.Statement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface StatementRepository extends JpaRepository<Statement, UUID> {
 }
