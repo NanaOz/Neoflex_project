8dd1d61e7321263ddfb0441d94909843a5e0f75c
Alina <alinavik93@gmail.com>
2025-07-16 11:33:03 +0500

    ошибки

---
+++ statment/src/main/java/com/javacode/statment/handler/PreScoringException.java
@@ -0,0 +1,8 @@
+package com.javacode.statment.handler;
+
+public class PreScoringException extends RuntimeException {
+
+    public PreScoringException(String ex) {
+        super(ex);
+    }
+}
---
+++ statment/src/main/java/com/javacode/statment/handler/ValidationException.java
@@ -0,0 +1,8 @@
+package com.javacode.statment.handler;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String ex) {
+        super(ex);
+    }
+}
