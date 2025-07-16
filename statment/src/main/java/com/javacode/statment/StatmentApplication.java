451b985e2b97878e3fb8b4803965f299346b78b5
Alina <alinavik93@gmail.com>
2025-07-16 11:34:36 +0500

    контроллер

---
+++ statment/src/main/java/com/javacode/statment/StatmentApplication.java
@@ -0,0 +1,13 @@
+package com.javacode.statment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatmentApplication.class, args);
+    }
+
+}
---
+++ statment/src/test/java/com/javacode/statment/StatmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacode.statment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
